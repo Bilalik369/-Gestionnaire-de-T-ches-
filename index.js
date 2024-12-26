a216193ee130f59aae33fcc01dd787924b6fe581
bilalik369 <bilal11iken@gmail.com>
2024-12-26 11:38:56 +0100

    Importer les bibliothèques essentielles, configurer une application Express, puis se connecter à une base de données MongoDB.

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express'); 
+const mongoose = require('mongoose');
+const taskRoutes = require('./routes/routes');
+
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017').then(() => {
+  console.log('connected  to mongodb ');
+})
+.catch(err => {
+  console.log('error concting to MongoDB');
+  
+});
+
+
+// app.use('/api', taskRoutes);
+
+const port = 3000;
+
+app.listen(port , () => {
+  console.log(`server running in port ,${port}`);
+});
+
